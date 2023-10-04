91586a0eb3ca1b5ed0c6b46a08728e13c56d532c
Ed Barberis <ed.barberis@gmail.com>
2023-10-04 14:05:54 -0400

    Upgraded Terraform to 1.6.0; AWS to 5.19; and EKS Blueprints Addons to 1.9.

--- examples/eks-cluster/versions.tf
+++ examples/eks-cluster/versions.tf
@@ -16,12 +16,12 @@
 
 # Terraform ----------------------------------------------------------------------------------------
 terraform {
-  required_version = ">= 1.5.7"
+  required_version = ">= 1.6.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.17"
+      version = ">= 5.19"
     }
 
     kubernetes = {
