0bffeba6a510f58d9925fb78dea7ee64a4931057
Ed Barberis <ed.barberis@gmail.com>
2023-10-09 11:03:46 -0400

    Upgraded Terraform to 1.6.0; AWS to 5.20; and EKS Blueprints Addons to 1.9.

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -111,7 +111,7 @@ module "vpc" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = ">= 19.16.0"
+  version = ">= 19.17.1"
 
   cluster_name    = local.cluster_name
   cluster_version = var.aws_eks_kubernetes_version
